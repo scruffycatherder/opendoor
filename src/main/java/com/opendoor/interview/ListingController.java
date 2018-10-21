a61c897bd0d1629c4e00769c9990be69e2dc3951
Bob Lewis <boblewis9@gmail.com>
2018-10-20 22:57:48 -0700

    .

--- src/main/java/com/opendoor/interview/ListingController.java
+++ src/main/java/com/opendoor/interview/ListingController.java
@@ -1,7 +1,7 @@
 package com.opendoor.interview;
 
 import java.sql.Connection;
-
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.SpringVersion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,14 +32,31 @@ public class ListingController {
 	String searchListings(
 			@RequestParam(value="geoLat") double geoLat,
 			@RequestParam(value="geoLon") double geoLon,
-			@RequestParam(value="radiusMeters", defaultValue="1609") int radiusMeters) {
+			@RequestParam(value="radiusMeters", defaultValue="1609") int radiusMeters,
+			@RequestParam(value="minBeds", defaultValue="0") int minBeds,
+			@RequestParam(value="maxBeds", defaultValue="-1") int maxBeds,
+			@RequestParam(value="minBaths", defaultValue="0") float minBaths,
+			@RequestParam(value="maxBaths", defaultValue="-1") float maxBaths,
+			@RequestParam(value="minArea", defaultValue="0") float minArea,
+			@RequestParam(value="maxArea", defaultValue="-1") float maxArea) {
 
 		try (Connection connection = dataSource.getConnection()) {
 
+			/*
 			Statement ps = connection.createStatement();
 			ResultSet results = ps.executeQuery(
 					String.format("select * from listings l where ST_DWithin(l.geoloc, 'POINT(%f %f)'::geography, %d);",
 					geoLat, geoLon, radiusMeters));
+			*/
+			// TODO query specific fields, not *
+			PreparedStatement ps = connection.prepareStatement(
+					String.format("select * from listings l where ST_DWithin(l.geoloc, 'POINT(%f %f)'::geography, %d) " +
+			                      "and l.numBedrooms > ? and l.numBaths > ? and l.livingArea > ?",
+					geoLat, geoLon, radiusMeters));
+			ps.setInt(1, minBeds);
+			ps.setFloat(2, minBaths);
+			ps.setFloat(3,  minArea);
+			ResultSet results = ps.executeQuery();
 			
 			// TODO : return JSON or protos
 			StringBuffer buf = new StringBuffer();
@@ -57,11 +73,16 @@ public class ListingController {
 				buf.append("<br>");
 				buf.append("Lon: " + results.getFloat(15) + System.lineSeparator());
 				buf.append("<br>");
+				buf.append("Bedrooms: " + results.getInt(18));
+				buf.append("<br>");
+				buf.append("Bathrooms: " + results.getFloat(19));
+				buf.append("<br>");
+				buf.append("LivingArea: " + results.getFloat(17));
+				buf.append("<br>");
 				buf.append("--------------------------");
 				buf.append("<br>");
 			}
 
-			buf.append("<br>Spring version: " + SpringVersion.getVersion());
 			buf.append("<br>");
 
 			return buf.toString();
